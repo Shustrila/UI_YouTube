7fc6f1e1516b86518828f227a6ac07e9b98d10c4
Alexandr Maxalov <sanya.maxalov@gmail.com>
2020-03-19 00:34:40 +0300

    Initial Commit

---
+++ youtube/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube
+//
+//  Created by Александр Махалов on 19.03.2020.
+//  Copyright © 2020 Александр Махалов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
